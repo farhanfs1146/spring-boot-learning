3c049fac45c5a4f9b63ebac5f3ebd926a2669603
Farhan <farhanaliforwardsports@gmail.com>
2026-01-29 12:00:35 +0500

    Employee Info Service Initial commit.

---
+++ src/main/java/com/spring_boot_learning/spring_boot_learning/service/impl/EmployeeInfoServiceImpl.java
@@ -0,0 +1,26 @@
+package com.spring_boot_learning.spring_boot_learning.service.impl;
+
+import com.spring_boot_learning.spring_boot_learning.service.EmployeeInfoService;
+
+public class EmployeeInfoServiceImpl implements EmployeeInfoService {
+
+    @Override
+    public void createNewEmployee() {
+
+    }
+
+    @Override
+    public void updateEmployeeById() {
+
+    }
+
+    @Override
+    public void getEmployeeById() {
+
+    }
+
+    @Override
+    public void deleteEmployeeById() {
+
+    }
+}
