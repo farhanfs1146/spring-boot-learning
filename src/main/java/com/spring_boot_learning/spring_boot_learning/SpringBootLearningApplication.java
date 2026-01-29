4241bd9ce196280c651f816670e7d767801b808a
Farhan <farhanaliforwardsports@gmail.com>
2026-01-29 10:11:31 +0500

    Initial commit

---
+++ src/main/java/com/spring_boot_learning/spring_boot_learning/SpringBootLearningApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_learning.spring_boot_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_boot_learning/spring_boot_learning/SpringBootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_learning.spring_boot_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
